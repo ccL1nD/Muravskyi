cace73995c20eab0cd05d85621a020caf0e69e02
ccL1nD <97873547+ccL1nD@users.noreply.github.com>
2023-11-01 02:54:38 +0700

    Все задания до 2 к.н.
    старался на совесть, питон знаю очень хорошо, хочу 5 кстати :D

---
+++ zadanie1.py
@@ -0,0 +1,11 @@
+
+for lp in range(101):
+    if lp == 100:
+
+        break
+
+    if lp < 100:
+        print(f"{lp}")
+
+    else:
+        print(f"{lp}")
---
+++ zadanie129.py
@@ -0,0 +1,5 @@
+number = int(input("Введите число в десятичной системе: "))
+
+    hex_number = hex(number)
+
+print(f"Число {number} в шестнадцатеричной системе: {hex_number}")
